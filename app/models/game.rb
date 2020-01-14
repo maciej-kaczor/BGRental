99df453c3ab919680369be1dd9d7a30e880c8463
maciej-kaczor <maciej.kaczor92@gmail.com>
2020-01-14 14:32:24 +0100

    Validation on adding and editing games, displaying errors, form moved to a partial

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,10 @@
 class Game < ApplicationRecord
     has_one_attached :picture
+
+    validates :name, presence: { message: "Pole wymagane" }
+    validates :deposit, numericality: { message: "Pole musi być liczbą" }
+    validates :rental_price, numericality: { message: "Pole musi być liczbą" }
+    validates :min_player_count, presence: { message: "Pole wymagane" }, numericality: { message: "Pole musi być liczbą" } 
+    validates :max_player_count, presence: { message: "Pole wymagane" }, numericality: { message: "Pole musi być liczbą" }
+    validates :publication_year, numericality: { message: "Pole musi być liczbą" }
 end
